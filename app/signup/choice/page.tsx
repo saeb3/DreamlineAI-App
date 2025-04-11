83bf5bc6fd29a662489d252c2b61afbefeda3f5b
Meet1903 <meet.diwan.md@gmail.com>
2025-04-11 13:40:38 -0400

    implement navigation for onboarding choices and update option handling

--- app/signup/choice/page.tsx
+++ app/signup/choice/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const DreamlineOnboarding = () => {
@@ -12,19 +12,29 @@ const DreamlineOnboarding = () => {
     setSelectedOption(option);
   };
 
+  const handleNext = (e: any) => {
+    e.preventDefault();
+    if (selectedOption === "property-owner") {
+      router.push("/signup/choice/property-owner");
+    } else if (selectedOption === "incentive-provider") {
+      router.push("/signup/choice/incentive-provider");
+    } else if (selectedOption === "contractor") {
+      router.push("/signup/choice/contractor");
+    }
+  };
+
   return (
     <div className="flex max-w-md min-h-screen flex-col bg-gray-50 text-black">
       <header className="flex items-center justify-between px-4 py-4 shadow-sm bg-white">
         <Image src="/images/logo.png" alt="Logo" width={80} height={50} />
-        <button className="block text-gray-600 md:hidden" aria-label="Menu">
-          <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path d="M4 5h16M4 12h16M4 19h16" />
-          </svg>
-        </button>
+        <button className="text-3xl">☰</button>
       </header>
 
       <div className="flex items-center p-4">
-        <span className="text-xl mr-4 hover:cursor-pointer" onClick={() => router.back()}>
+        <span
+          className="text-xl mr-4 hover:cursor-pointer"
+          onClick={() => router.back()}
+        >
           <Image
             src="/images/signup/back_arrow.png"
             alt="Signup Page Logo"
@@ -72,9 +82,11 @@ const DreamlineOnboarding = () => {
       <div className="p-4 space-y-4">
         <div
           className={`flex items-center justify-between p-4 border rounded-lg bg-white cursor-pointer ${
-            selectedOption === "home" ? "border-blue-500" : "border-gray-200"
+            selectedOption === "property-owner"
+              ? "border-blue-500"
+              : "border-gray-200"
           }`}
-          onClick={() => handleOptionClick("home")}
+          onClick={() => handleOptionClick("property-owner")}
         >
           <div className="flex items-center">
             <span className="mr-4 text-xl">
@@ -89,10 +101,12 @@ const DreamlineOnboarding = () => {
           </div>
           <div
             className={`w-6 h-6 rounded-full border-2 flex items-center justify-center ${
-              selectedOption === "home" ? "border-blue-500" : "border-gray-300"
+              selectedOption === "property-owner"
+                ? "border-blue-500"
+                : "border-gray-300"
             }`}
           >
-            {selectedOption === "home" && (
+            {selectedOption === "property-owner" && (
               <div className="w-3 h-3 rounded-full bg-blue-500"></div>
             )}
           </div>
@@ -100,15 +114,15 @@ const DreamlineOnboarding = () => {
 
         <div
           className={`flex items-center justify-between p-4 border rounded-lg bg-white cursor-pointer ${
-            selectedOption === "incentives"
+            selectedOption === "incentive-provider"
               ? "border-blue-500"
               : "border-gray-200"
           }`}
-          onClick={() => handleOptionClick("incentives")}
+          onClick={() => handleOptionClick("incentive-provider")}
         >
           <div className="flex items-center">
             <span className="mr-4 text-xl">
-            <Image
+              <Image
                 src="/images/signup/incentive_vector.png"
                 alt="Home Icon"
                 width={24}
@@ -119,12 +133,12 @@ const DreamlineOnboarding = () => {
           </div>
           <div
             className={`w-6 h-6 rounded-full border-2 flex items-center justify-center ${
-              selectedOption === "incentives"
+              selectedOption === "incentive-provider"
                 ? "border-blue-500"
                 : "border-gray-300"
             }`}
           >
-            {selectedOption === "incentives" && (
+            {selectedOption === "incentive-provider" && (
               <div className="w-3 h-3 rounded-full bg-blue-500"></div>
             )}
           </div>
@@ -167,6 +181,7 @@ const DreamlineOnboarding = () => {
         <button
           type="submit"
           className="mt-4 w-full rounded-2xl bg-blue-600 px-4 py-2 font-semibold text-white hover:bg-blue-700 cursor-pointer focus:outline-none"
+          onClick={handleNext}
         >
           Next
         </button>
